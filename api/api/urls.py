7d60b6a701d0ac8e7b3f6c2cfa44bf3f0b69b071
laurentlannareix <lannareix.l@outlook.com>
2022-11-17 10:13:05 +0100

    Update urls.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
+    path("logout/", views.logout, name="logout"),
     path("callback", views.callback, name="callback"),
 ]
